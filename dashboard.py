71b7654957b94042db98d84a08007c7197aa5c64
mimarcele <milenamarcele777@gmail.com>
2023-07-01 15:53:08 -0300

    incluindo outra competicao e time

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,3 @@
-
 # visit http://127.0.0.1:8050/ in your web browser.
 
 from dash import Dash, html, dcc, Input, Output
@@ -8,9 +7,11 @@ import pandas as pd
 app = Dash(__name__)
 
 df = pd.DataFrame({
-    "Times": ["Flamengo", "Flamengo", "Flamengo", "Corinthians",  "Corinthians", "Corinthians", "Fluminense", "Fluminense", "Fluminense", "São Paulo"],
-    "Gols": [20, 16, 30, 3, 9, 10, 0, 2, 8, 5],
-    "Competições": ["Libertadores", "Copa do Brasil", "Brasileirão", "Libertadores", "Copa do Brasil", "Brasileirão", "Libertadores", "Copa do Brasil", "Brasileirão", "Brasileirão"]
+    "Times": ["Flamengo", "Flamengo", "Flamengo", "Corinthians", "Corinthians", "Corinthians", "Fluminense",
+              "Fluminense", "Fluminense", "São Paulo", "Vasco da Gama"],
+    "Gols": [20, 16, 30, 3, 9, 10, 0, 2, 8, 5, 5],
+    "Competições": ["Libertadores", "Copa do Brasil", "Brasileirão", "Libertadores", "Copa do Brasil", "Brasileirão",
+                    "Libertadores", "Copa do Brasil", "Brasileirão", "Brasileirão", "Série B"]
 })
 
 fig = px.bar(df, x="Times", y="Gols", color="Competições", barmode="group")
@@ -22,7 +23,6 @@ app.layout = html.Div(children=[
         Dashboard Esportivo
     '''),
 
-
     dcc.Dropdown(
         id='dropdown',
         options=[
@@ -30,7 +30,8 @@ app.layout = html.Div(children=[
             {'label': 'Flamengo', 'value': 'FLA'},
             {'label': 'Corinthians', 'value': 'SCCP'},
             {'label': 'Fluminense', 'value': 'FLU'},
-            {'label': 'São Paulo', 'value': 'SPFC'}
+            {'label': 'São Paulo', 'value': 'SPFC'},
+            {'label': 'Vasco da Gama', 'value': 'CRVG'}
         ],
         value='FLA',
     ),
@@ -41,19 +42,22 @@ app.layout = html.Div(children=[
     )
 ])
 
+
 @app.callback(
     Output(component_id='example-graph', component_property='figure'),
     Input(component_id='dropdown', component_property='value')
 )
 def changeText(value):
-    if(value) == 'FLA':
+    if (value) == 'FLA':
         return px.bar(df[df['Times'] == 'Flamengo'], x="Times", y="Gols", color="Competições")
-    elif(value) == 'FLU':
+    elif (value) == 'FLU':
         return px.bar(df[df['Times'] == 'Fluminense'], x="Times", y="Gols", color="Competições")
-    elif(value) == 'SCCP':
+    elif (value) == 'SCCP':
         return px.bar(df[df['Times'] == 'Corinthians'], x="Times", y="Gols", color="Competições")
-    elif(value) == 'SPFC':
+    elif (value) == 'SPFC':
         return px.bar(df[df['Times'] == 'São Paulo'], x="Times", y="Gols", color="Competições")
+    elif (value) == 'CRVG':
+        return px.bar(df[df['Times'] == 'Vasco da Gama'], x="Times", y="Gols", color="Competições")
     else:
         return px.bar(df, x="Times", y="Gols", color="Competições", barmode="group")
 
